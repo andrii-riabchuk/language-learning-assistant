22c4f9a931a8f0771945071a95f86a45d65a1178
Andrii Riabchuk <andrii.riabchuk@portnox.com>
2023-11-30 18:24:46 +0200

    added chatgpt client

---
+++ backend/chatgpt_client.py
@@ -0,0 +1,32 @@
+from openai import OpenAI
+from priming import Role, create_message, request_entry, get_priming_messages
+
+import json
+from types import SimpleNamespace
+
+MODEL="gpt-3.5-turbo"
+
+class ChatGPTClient:
+    def __init__(self) -> None:
+        self.client = OpenAI()
+    
+    def requestDefinitions(self, words):
+        messages = get_priming_messages()
+
+        list(map(lambda wc: request_entry(wc[0], wc[1]), words))
+        user_message = f'[{','.join(list(map(lambda wc: request_entry(wc[0], wc[1]), words)))}]'
+        
+        messages.append(create_message(Role.User, user_message))
+        # print('trying to send such messages:')
+        # for message in messages:
+        #     print(message,type(message), end='\n\n')
+
+        res = self.client.chat.completions.create(model=MODEL,messages=messages)
+        print(f'{len(res.choices)} choice(s) - picking first')
+
+        first = res.choices[0]
+        data = first.message.content
+        obj = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+        
+        return obj
+
---
+++ backend/priming.py
@@ -0,0 +1,47 @@
+
+class Role:
+    System = 'system'
+    User = 'user'
+    Assistant = 'assistant'
+
+
+def create_message(role, content):
+    return {"role": role, "content": content}
+
+def request_entry(word, context):
+    return [word, context].__str__()
+
+def example_noun():
+    word, context = 'courriel', 'Courriel pour des renseignements'
+    request = request_entry(word, context)
+    response = '{"base": "courriel", "definition":"email", "pos":"noun", "gender":"m"}'
+    return (request, response)
+
+def example_verb():
+    word, context = 'j’aimerais', 'J’aimerais aller en Écosse.'
+    request = request_entry(word, context)
+    response = '{"base": "aimer", "definition":"would like", "pos":"verb"}'
+    return (request, response)
+
+def example_preposition():
+    word, context = 'sur', 'Pose-le sur la table'
+    request = request_entry(word, context)
+    response = '{"base": "sur", "definition":"on", "pos":"preposition"}'
+    return (request, response)
+
+def get_priming_messages():
+    system = create_message(Role.System, "You act like a French dictionary API, I give list of entries in format [word, sentence with this word for context] for each")
+
+    requests, responses = [], []
+    for example in [example_noun(), example_verb(), example_preposition()]:
+        request, response = example
+        requests.append(request)
+        responses.append(response)
+    
+    user_message = f'[{','.join(requests)}]'
+    user = create_message(Role.User, user_message)
+
+    assistant_message = f'[{','.join(responses)}]'
+    assistant = create_message(Role.Assistant, assistant_message)
+
+    return [system, user, assistant]
